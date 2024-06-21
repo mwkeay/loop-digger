1f7fd86f35aaea4ab216f1dd3d2a619d80e40854
= <mwkeay@gmail.com>
2024-06-21 23:54:22 +0100

    Styled input icons and spellcheck

--- app/src/components/StyledInput.tsx
+++ app/src/components/StyledInput.tsx
@@ -1,17 +1,20 @@
 "use client";
 
-import { ChangeEvent, FC, useRef, useState } from "react";
+import { ChangeEvent, FC, FunctionComponent, SVGProps, useRef, useState } from "react";
 import "@/styles/styled-input.css";
+import Image from "next/image";
 
 interface StyledInputProps {
     callback: (value: string) => void
     errorMessage?: string
     filter?: (value: string) => boolean
+    iconPath?: string
     id?: string
     label?: string
+    spellCheck?: boolean
 }
 
-const StyledInput: FC<StyledInputProps> = ({ callback, errorMessage, filter, id, label }) => {
+const StyledInput: FC<StyledInputProps> = ({ callback, errorMessage, filter, iconPath, id, label, spellCheck = false }) => {
 
     const [value, setValue] = useState("");
 
@@ -33,17 +36,32 @@ const StyledInput: FC<StyledInputProps> = ({ callback, errorMessage, filter, id,
     };
 
     return (
-        <div className={ "styled-input" + (errorMessage ? " styled-input-error" : "") }>
+        <div className={
+            "styled-input"
+            + (errorMessage ? " styled-input-error" : "")
+            + (iconPath ? " styled-input-icon-indent" : "")
+        }>
 
             {
                 label
                     && <label ref={ labelRef } htmlFor={ id }>{ label }</label>
             }
 
+            {
+                iconPath && <div>
+                    <Image
+                        src={ iconPath }
+                        layout="fill"
+                        alt={ iconPath }
+                    />
+                </div>
+            }
+
             <input
                 id={ id }
                 value={ value }
                 onChange={ handleChange }
+                spellCheck={ spellCheck }
             />
 
             {
@@ -64,6 +82,7 @@ export const DummyStyledInput = () => {
             callback={ (value: string) => value.includes("ERROR") ? setIsError(true) : setIsError(false) }
             errorMessage={ isError ? "Error: This didn't work." : undefined }
             filter={ (value: string) => !/\d/.test(value) }
+            iconPath="/images/search-icon-white.svg"
             id="dummy-styled-input"
             label="This is a really fucking long input"
         />
